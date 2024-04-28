58adec031df9991cd7bc9096fc6a9cc42e4c9549
driftluo <driftluo@foxmail.com>
2024-04-28 15:58:56 +0800

    feat: support custom union id

--- pkg/generator/types.go
+++ pkg/generator/types.go
@@ -14,17 +14,27 @@ type Field struct {
 }
 
 type Declaration struct {
-	Type      string   `json:"type"`
-	Name      string   `json:"name"`
-	Item      string   `json:"item"`
-	Items     []string `json:"items"`
-	ItemCount int      `json:"item_count"`
-	Fields    []Field  `json:"fields"`
+	Type      string       `json:"type"`
+	Name      string       `json:"name"`
+	Item      string       `json:"item"`
+	Items     []UnionItems `json:"items"`
+	ItemCount int          `json:"item_count"`
+	Fields    []Field      `json:"fields"`
+}
+
+type UnionItems struct {
+	Type string `json:"typ"`
+	Id   uint64 `json:"id"`
+}
+
+type SyntaxVersion struct {
+	Version uint64 `json:"version"`
 }
 
 type Schema struct {
-	Namespace    string        `json:"namespace"`
-	Declarations []Declaration `json:"declarations"`
+	SyntaxVersion SyntaxVersion `json:"syntax_version"`
+	Namespace     string        `json:"namespace"`
+	Declarations  []Declaration `json:"declarations"`
 }
 
 func (s Schema) FindDeclaration(itemName string) (Declaration, error) {
